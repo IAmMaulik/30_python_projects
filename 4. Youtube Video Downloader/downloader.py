07f4d0fdc290263e35914110d30f325b50298d9d
Maulik Shah <maulik.shah715@gmail.com>
2020-12-18 12:18:15 +0530

    Fixed the bug that was not allowing users to donwload playlist

--- 4. Youtube Video Downloader/downloader.py
+++ 4. Youtube Video Downloader/downloader.py
@@ -2,38 +2,37 @@ print("Welcome to Maulik Shah's YouTube Downloader")
 print("Loading...")
 
 import pytube
-import youtube_downloader
-import file_converter
 
-print('''
-What do you want?
-(1) Download YouTube Videos Manually
-(2) Download a YouTube Playlist
-(3) Download YouTube Videos and Convert Into MP3
-Downloading copyrighted YouTube videos is illegal!
-I am not responsible for your downloads! Go at your own risk!
-Copyright (c) Maulik Shah 2020
-''')
+video_list = []
 
-choice = input("Choice: ")
+a = int(input("Enter your Choice\n(1)Download manually\n(2)Download entire playlist\n"))
 
-if choice == "1" or choice == "2":
-    quality = input("Please choose a quality (low, medium, high, very high):")
-    if choice == "2":
-        link = input("Enter the link to the playlist: ")
-        print("Downloading playlist...")
-        youtube_downloader.download_playlist(link, quality)
-        print("Download finished!")
-    if choice == "1":
-        links = youtube_downloader.input_links()
-        for link in links:
-            youtube_downloader.download_video(link, quality)
-elif choice == "3":
-    links = youtube_downloader.input_links()
-    for link in links:
-        print("Downloading...")
-        filename = youtube_downloader.download_video(link, 'low')
-        print("Converting...")
-        file_converter.convert_to_mp3(filename)
-else:
-    print("Invalid input! Terminating...")
+if a==1:
+    print("Enter the urls. Write STOP to terminate")
+
+    while True:
+        url = input("Enter the URL: ")        
+        if url.lower()=='stop':
+            break
+        video_list.append(url)
+
+    for x, video in enumerate(video_list):
+        v = pytube.YouTube(video)
+        stream = v.streams.get_by_itag(134)
+        print(f"Downloading video {x}")
+        stream.download()
+        print("Done!\n")
+
+if a==2:
+    playlist = input("Enter the playlist link: ")
+    p = pytube.Playlist(playlist)
+
+    for i in p:
+        video = pytube.YouTube(playlist)
+        print(f"Downloading video {x+1}")
+        stream = video.streams.get_by_itag(134)
+        stream.download()
+        print("Done")
+
+if a>2 or a<1:
+    print("Invalid request")
