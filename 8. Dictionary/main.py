1f822e5106132b2301b8b6a2429a480d829a2856
Maulik Shah <maulik.shah715@gmail.com>
2020-12-27 13:52:37 +0530

    Made a fully functioning dictionary

--- 8. Dictionary/main.py
+++ 8. Dictionary/main.py
@@ -1,12 +1,27 @@
 import json
+from difflib import get_close_matches
 
 def find(w):
-    try:
+    if w in data:
         return data[word.lower()]
-    except:
-        return("That word does not exist. Please try again")
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yon = input(f"That word doesn't exist. Did you mean {get_close_matches(w, data.keys())[0]} instead? Enter Y is Yes or N if no:  ")
+        if yon.lower() == 'y':
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yon.lower() == 'n':
+            return "That word does not exist. Please try again..."
+        else:
+            return f"{yon} is not a valid input. Exiting Program..."
+    else:
+        return "That word does not exist. Please try again..."
 
 data = json.load(open("data.json"))
 
 word = input("Enter the word: ")
-print(find(word))
+output = (find(word))
+i = 1
+
+if type(output) == list:
+    for item in output:
+        print(f"{i}. {item}")
+        i += 1
